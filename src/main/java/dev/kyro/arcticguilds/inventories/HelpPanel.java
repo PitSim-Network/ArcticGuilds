9a44172b5ee0d3358bc1c1278b37f91c20a5365b
KyroKrypt <kyrokrypt@gmail.com>
2022-02-06 13:39:02 -0800

    guild help menu

--- src/main/java/dev/kyro/arcticguilds/inventories/HelpPanel.java
+++ src/main/java/dev/kyro/arcticguilds/inventories/HelpPanel.java
@@ -4,6 +4,7 @@ import dev.kyro.arcticapi.builders.AItemStackBuilder;
 import dev.kyro.arcticapi.builders.ALoreBuilder;
 import dev.kyro.arcticapi.gui.AGUI;
 import dev.kyro.arcticapi.gui.AGUIPanel;
+import dev.kyro.arcticguilds.misc.Constants;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -23,7 +24,10 @@ public class HelpPanel extends AGUIPanel {
 		ItemStack general = new AItemStackBuilder(Material.PAPER)
 				.setName("&f&lWhat are Guilds?")
 				.setLore(new ALoreBuilder(
-
+						"&7Guilds allow players to group up",
+						"&7to fight, streak, and hang out",
+						"",
+						"&7Being a part of a guild comes with"
 				))
 				.getItemStack();
 		getInventory().setItem(10, general);
@@ -31,7 +35,18 @@ public class HelpPanel extends AGUIPanel {
 		ItemStack reputation = new AItemStackBuilder(Material.DIAMOND)
 				.setName("&b&lReputation System")
 				.setLore(new ALoreBuilder(
-
+						"&7Guild reputation measures the strength",
+						"&7of a guild and can be earned from a",
+						"&7variety of tasks",
+						"",
+						"&7Each 1,000 reputation gives a reputation point",
+						"&7which can be used to activate specific buffs",
+						"",
+						"&7Reputation Sources:",
+						"&7 * Being active",
+						"&7 * Killing other guilds",
+						"&7 * Making other guilds lose feathers",
+						"&7 * Streaking (WIP--NOT IMPLEMENTED)"
 				))
 				.getItemStack();
 		getInventory().setItem(11, reputation);
@@ -39,7 +54,12 @@ public class HelpPanel extends AGUIPanel {
 		ItemStack upgrades = new AItemStackBuilder(Material.ANVIL)
 				.setName("&2&lUpgrades")
 				.setLore(new ALoreBuilder(
-
+						"&7Unlike reputation buffs, upgrades are permanent.",
+						"&7Upgrades can be found in the upgrade GUI and improve",
+						"&7your guild in many ways",
+						"",
+						"&7To purchase an upgrade, have a " + Constants.UPGRADES_PERMISSION.displayName + " purchase a upgrade",
+						"&7and ensure that there is enough money in the guild bank"
 				))
 				.getItemStack();
 		getInventory().setItem(12, upgrades);
@@ -47,26 +67,25 @@ public class HelpPanel extends AGUIPanel {
 		ItemStack rewards = new AItemStackBuilder(Material.FEATHER)
 				.setName("&e&lRewards")
 				.setLore(new ALoreBuilder(
-
+						"&7Rewards will most likely be given out",
+						"&7in the form of feathers to the best performing",
+						"&7guilds (Feature WIP)"
 				))
 				.getItemStack();
 		getInventory().setItem(13, rewards);
 
-		ItemStack combat = new AItemStackBuilder(Material.DIAMOND_SWORD)
-				.setName("&9&lCombat")
-				.setLore(new ALoreBuilder(
-
-				))
-				.getItemStack();
-		getInventory().setItem(14, combat);
-
 		ItemStack joinLeave = new AItemStackBuilder(Material.HOPPER)
 				.setName("&7&lJoining and Leaving")
 				.setLore(new ALoreBuilder(
-
+						"&7Joining, leaving, creating, and disbanding are",
+						"&7all actions that have a cooldown. This ensures",
+						"&7that players don't guild hop.",
+						"",
+						"Reference the /guild command",
+						"&7to see commands for these actions"
 				))
 				.getItemStack();
-		getInventory().setItem(15, joinLeave);
+		getInventory().setItem(14, joinLeave);
 	}
 
 	@Override
